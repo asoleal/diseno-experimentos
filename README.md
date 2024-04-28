fb05716f0b420368ceb3e79bc199f20cd06c43dc
John Leal <jlealgom@unal.edu.co>
2024-04-28 09:26:25 -0500

    configuracion inial, subiendo archivos basicos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# diseno-experimentos
+# Métodos Numéricos
+En ésta página encontrará teoría, ejemplos y ejercicios correspondientes al curso de métodos numéricos para ingeniería.  
---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CURSO DE MÉTODOS NUMÉRICOS'
+copyright = '2024, John Jairo Leal Gómez'
+author = 'John Jairo Leal Gómez'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode','sphinx.ext.mathjax']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'es'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('custom.css')
